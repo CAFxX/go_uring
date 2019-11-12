73c823595141d95f75b49b46930ee14984d6c75c
Carlo Alberto Ferraris <cafxx@mercari.com>
2019-11-12 09:15:15 +0900

    Update iouring.go

--- iouring.go
+++ iouring.go
@@ -43,6 +43,7 @@ type opType int8
 const (
 	opRead opType = iota
 	opWrite
+	opAppend
 	opSync
 )
 
@@ -89,25 +90,37 @@ func (r *iouring) putsv(sv []sche) {
 			}
 			// C.io_uring_prep_readv(sqe, s.fd, &iovecs[si], 1, s.pos)
 			sqe.opcode = C.IORING_OP_READV
-			// sqe.off = s.pos
-			*(*C.ulong)(unsafe.Pointer(&sqe.anon0[0])) = C.ulong(s.pos)
+			*(*C.ulong)(unsafe.Pointer(&sqe.anon0[0])) = C.ulong(s.pos) // sqe.off = s.pos
 			sqe.addr = C.ulonglong(uintptr(unsafe.Pointer(&iovecs[si])))
 			sqe.len = 1
+
 		case opWrite:
 			if len(s.buf) > 0 {
 				iovecs[si] = C.struct_iovec{unsafe.Pointer(&s.buf[0]), C.ulong(len(s.buf))}
 			}
 			// C.io_uring_prep_writev(sqe, s.fd, &iovecs[si], 1, s.pos)
 			sqe.opcode = C.IORING_OP_WRITEV
-			// sqe.off = s.pos
-			*(*C.ulong)(unsafe.Pointer(&sqe.anon0[0])) = C.ulong(s.pos)
+			*(*C.ulong)(unsafe.Pointer(&sqe.anon0[0])) = C.ulong(s.pos) // sqe.off = s.pos
 			sqe.addr = C.ulonglong(uintptr(unsafe.Pointer(&iovecs[si])))
 			sqe.len = 1
+
+		case opAppend:
+			if len(s.buf) > 0 {
+				iovecs[si] = C.struct_iovec{unsafe.Pointer(&s.buf[0]), C.ulong(len(s.buf))}
+			}
+			// C.io_uring_prep_writev(sqe, s.fd, &iovecs[si], 1, 0)
+			// 
+			sqe.opcode = C.IORING_OP_WRITEV
+			sqe.addr = C.ulonglong(uintptr(unsafe.Pointer(&iovecs[si])))
+			sqe.len = 1
+			*(*C.__kernel_rwf_t)(unsafe.Pointer(&sqe.anon1[0])) = C.RWF_APPEND // sqe.rw_flags = RWF_APPEND
+
 		case opSync:
 			// C.io_uring_prep_fsync(sqe, s.fd, s.flags)
 			sqe.opcode = C.IORING_OP_FSYNC
 			// sqe.fsync_flags = s.flags
 			*(*C.uint)(unsafe.Pointer(&sqe.anon1[0])) = C.uint(s.flags)
+
 		default:
 			panic("unknown opType")
 		}
@@ -158,6 +171,12 @@ func (r *iouring) WriteFile(f *os.File, buf []byte, pos int64) (int64, error) {
 	return r.submitAndWait(opWrite, f, buf, pos, 0)
 }
 
+// AppendFile writes the contents of buf appending to file f.
+// Returns the number of bytes written, or an error.
+func (r *iouring) AppendFile(f *os.File, buf []byte) (int64, error) {
+	return r.submitAndWait(opAppend, f, buf, 0, 0)
+}
+
 // SyncFile performs an fsync with the specified flags on file f.
 func (r *iouring) SyncFile(f *os.File, flags uint) error {
 	_, err := r.submitAndWait(opSync, f, nil, 0, flags)
@@ -178,11 +197,18 @@ func ReadFile(f *os.File, buf []byte, pos int64) (int64, error) {
 	return global.ReadFile(f, buf, pos)
 }
 
-// WriteFile writes the contents of buf in file f starting from offset pos.
+// WriteFile writes the contents of buf appended to file f.
 // Returns the number of bytes written, or an error.
-func WriteFile(f *os.File, buf []byte, pos int64) (int64, error) {
+func WriteFile(f *os.File, buf []byte) (int64, error) {
 	once.Do(global.Init)
-	return global.WriteFile(f, buf, pos)
+	return global.WriteFile(f, buf)
+}
+
+// AppendFile writes the contents of buf in file f starting from offset pos.
+// Returns the number of bytes written, or an error.
+func AppendFile(f *os.File, buf []byte, pos int64) (int64, error) {
+	once.Do(global.Init)
+	return global.AppendFile(f, buf, pos)
 }
 
 // SyncFile performs an fsync with the specified flags on file f.
